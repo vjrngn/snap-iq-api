a0dafd83e59ac29222dc5f3cd64488125fca0b83
Ankur Heble <ankurheble91@gmail.com>
2018-09-01 12:56:07 +0530

    Updated app.js to use cors and mongoose

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var mongoose = require('mongoose');
+
+const { HOST = "localhost", PORT = 27017 } = process.env;
+
+mongoose.connect(
+  `mongodb://${HOST}:${PORT}/snap-iq-api`,
+  function(error) {
+    if (!error) {
+      console.log("Successfully connected to monogoDb.");
+    }
+  }
+);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
